197608e88b9c1bc217c9864595444f6047b6bb00
AlexHaxe <Alexander.Blum@gmail.com>
2020-04-13 21:09:42 +0200

    run some targets twice to smooth out their graphs

--- benchmark.sh
+++ benchmark.sh
@@ -19,3 +19,15 @@ neko out/AllocMain.n $DATA
 python3 out/AllocMain.py $DATA
 # resetGit
 # lua out/formatter.lua $DATA
+
+# second run to smooth graphs a little
+./out/cpp/AllocMain $DATA
+./out/cppGCGen/AllocMain $DATA
+node out/AllocMain.js $DATA
+node out/AllocMain.es6.js $DATA
+java -jar out/java/AllocMain.jar $DATA
+java -jar out/jvm/AllocMain.jar $DATA
+hl out/AllocMain.hl $DATA
+./out/hl/AllocMain $DATA
+mono out/cs/bin/AllocMain.exe $DATA
+haxe build/common.hxml --run AllocMain $DATA
